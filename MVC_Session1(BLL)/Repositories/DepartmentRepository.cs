98de643d5ac444f666213474a3a7643b1ee65b01
RahmaMohamed <120463022+RahmaMuhammed@users.noreply.github.com>
2024-03-20 22:08:43 +0200

    3-Layers Arch. Pattern - Business Logic Layer

---
+++ MVC_Session1(BLL)/Repositories/DepartmentRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Session1_BLL_.Interfaces;
+using MVC_Session1_DAL_.Data;
+using MVC_Session1_DAL_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Session1_BLL_.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext) // ask CLR for Creating Object from "AppDbContext" 
+        {
+            // dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>);
+            _dbContext = dbContext;
+        }
+        public int Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+             /// var department = _dbContext.Departments.Local.Where(D => D.Id == id).FirstOrDefault();
+            /// if(department == null)
+            ///      department = _dbContext.Departments.Where(D => D.Id == id).FirstOrDefault();
+
+            return _dbContext.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+            => _dbContext.Departments.AsNoTracking().ToList();
+
+    }
+}
