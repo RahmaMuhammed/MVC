cac72a4e643ba43f70bb2d27032f856e58f99b9c
RahmaMohamed <120463022+RahmaMuhammed@users.noreply.github.com>
2024-03-23 03:32:10 +0200

    Create Details View

--- MVC_Session1(PL)/Controllers/DepartmentController.cs
+++ MVC_Session1(PL)/Controllers/DepartmentController.cs
@@ -27,16 +27,33 @@ namespace MVC_Session1_PL_.Controllers
         [HttpPost]
         public IActionResult Create(Department department)
         {
-            if(ModelState.IsValid) // server side validation
+            if (ModelState.IsValid) // server side validation
             {
                 var count = _departmentRepositery.Add(department);
-                
-                if(count > 0)
+
+                if (count > 0)
                 {
                     return RedirectToAction(nameof(Index));
                 }
             }
             return View(department);
+        }
+
+
+        // Department/Details/10
+        // Department/Details
+        [HttpGet]
+        public IActionResult Details(int? Id)
+        {
+            if (Id is null)
+              return BadRequest(); // 400
+
+            var department = _departmentRepositery.Get(Id.Value);
+            if(department == null)
+                return NotFound(); // 404
+
+            return View(department);
+
         }
     }
 }
